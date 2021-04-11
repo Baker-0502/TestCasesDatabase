fde54b2b184523f4e176fb635462f9cd2c6761ea
Baker-0502 <bakerdq@email.sc.edu>
2021-04-10 22:44:08 -0400

    added tests, added new test person

--- src/CriminalDatabaseApplicationTester.java
+++ src/CriminalDatabaseApplicationTester.java
@@ -12,6 +12,15 @@ public class CriminalDatabaseApplicationTester {
     static void setup() {
         db.createAdmin(UUID.randomUUID(), "Randy", "Briggs", "rbriggs", "IAmATest", "rbriggs@gmail.com", "8023949949", "Sumter County", true);
         db.createWitness(UUID.randomUUID(), "Blop", "Whoo", "Male", "White", 19, 62.6, 167.4, "8435051109", "1234 blue st", "worker", "NA", "saw accident and shooting");
+        //(UUID personID, String firstName, String lastName, String gender, String race, int age, double height, double weight, String phoneNumber, String address, String occupation, String hairColor, 
+        //String eyeColor, String footSize, String bloodType, String fingerPrint, String details, ArrayList<String> clothing, ArrayList<String> tattoos)
+        ArrayList<String> clothesPersonTest = new ArrayList<String>();
+        ArrayList<String> tattoosPersonTest = new ArrayList<String>();
+        clothesPersonTest.add("Big Jacket");
+        tattoosPersonTest.add("red heart");
+        tattoosPersonTest.add("yellow dove");
+
+        db.createSuspect(UUID.randomUUID(), "Tattoo", "man", "male", "white", 21, 129, 233, "938241341123", "029 awdij lane", "tattoo man", "brown", "blue", "12m", "A+", "Whorl", "ihuaawdkjnawdoiijoikj", , tattoos);
         db.createAdmin(UUID.randomUUID(), "", "", "", "", "", "", " ", false);
         db.createVictim(UUID.randomUUID(), "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïð","ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïð","B","C", 0, 22, 22, "ADWAWD", "AWDAWDAWDAWD", "ZXCZXCZXC", "IOUHAWFIDJBHN", "OIHJAo:iwJDOIJAWD\t\t\t0");
         db.saveAll();
@@ -38,7 +47,7 @@ public class CriminalDatabaseApplicationTester {
     }
 
     //searchFirst Tests
-    //ADDED FIX, ALL ADD METHODS NEED TO ADD TO PERSON LIST. FIX BEFORE CHANGE
+    //ADDED FIX, ALL ADD METHODS NEED TO ADD TO PERSON LIST. FIX IN ACTUAL CODE BEFORE CHANGE
     @Test
     void testSearchFirst_Null() {
         assertEquals(db.searchFirst(null), new ArrayList<Person>());
@@ -59,5 +68,28 @@ public class CriminalDatabaseApplicationTester {
     @Test
     void testSearchFirst_Correct() {
         assertTrue(db.searchFirst("Blop").get(0) instanceof Person);
+    }
+
+    //test SearchTattoo
+    @Test
+    void testSearchTattoo_Null() {
+        assertEquals(db.searchTattoo(null), new ArrayList<Person>());
+    }
+
+    @Test
+    void testSearchTattoo_ASCII() {
+        Person personAscii = db.searchTattoo("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïð").get(0);
+        assertTrue(personAscii instanceof Person);
+    }
+
+    @Test
+    void testSearchTattoo_Incorrect() {
+        ArrayList<Person> peopleFound = db.searchTattoo("iuhawiudjpojmolnmaiwuidn");
+        assertTrue(peopleFound.size() == 0);
+    }
+
+    @Test
+    void testSearchTattoo_Correct() {
+        assertTrue(db.searchTattoo("red heart").get(0) instanceof Person);
     }
 }
